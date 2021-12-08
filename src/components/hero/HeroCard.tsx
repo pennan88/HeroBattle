0fdc7c227ceb2fae4af0ea24165266d54790b629
pennan88 <erikalexander98@hotmail.com>
2021-12-08 13:20:06 +0100

    powerlevel added

--- src/components/hero/HeroCard.tsx
+++ src/components/hero/HeroCard.tsx
@@ -37,10 +37,15 @@ export const HeroCard: React.FC<iProps> = ({ id, variant }) => {
     const dur = parseInt(DUR);
     const pwr = parseInt(PWR);
     const cmb = parseInt(CMB);
-    const result = Math.pow(int, 1.5) + Math.pow((str * 0.5), 2) + Math.pow(spd * 0.5, 2) + Math.pow(dur, 1.6) + Math.pow(pwr + (3*6), 2) + Math.pow(cmb, 1.8)
-    const final = Math.floor(result)
-    return final
-    
+    const result =
+      Math.pow(int, 1.5) +
+      Math.pow(str * 0.5, 2) +
+      Math.pow(spd * 0.5, 2) +
+      Math.pow(dur, 1.6) +
+      Math.pow(pwr + 3 * 6, 2) +
+      Math.pow(cmb, 1.8);
+    const final = Math.floor(result);
+    return final;
   };
 
   useEffect(() => {
@@ -54,9 +59,27 @@ export const HeroCard: React.FC<iProps> = ({ id, variant }) => {
       </div>
       <div className="backgroundFiller"></div>
       <div className="heroTopHalf">
-        <p className="heroName">{hero?.name}</p>
-        <p style={{color : "white"}}>{powerlevel(powerLevel?.strength, powerLevel?.intelligence, powerLevel?.speed, powerLevel?.durability, powerLevel?.power, powerLevel?.combat)}</p>
-        <p className="heroAlias">{hero?.biography?.aliases[0]}</p>
+        <div className="powerLevel">
+          <p>powerlevel</p>
+          <h4>
+            {powerlevel(
+              powerLevel?.strength,
+              powerLevel?.intelligence,
+              powerLevel?.speed,
+              powerLevel?.durability,
+              powerLevel?.power,
+              powerLevel?.combat
+            )}
+          </h4>
+        </div>
+        <div className="heroName">
+          <p>Hero name</p>
+          <h4>{hero?.name}</h4>
+        </div>
+        <div className="heroAlias">
+          <p>Alias</p>
+          <h4>{hero?.biography?.aliases[0]}</h4>
+        </div>
       </div>
       <div className="heroBottomHalf">
         <div className="heroDescription">
@@ -65,10 +88,20 @@ export const HeroCard: React.FC<iProps> = ({ id, variant }) => {
               Gender: <p>{hero?.appearance?.gender}</p>
             </span>
             <span>
-              Height: <p>{hero?.appearance?.height?.[1] === "0 cm" ? "Unknown height" : hero?.appearance?.height?.[1]}</p>
+              Height:{" "}
+              <p>
+                {hero?.appearance?.height?.[1] === "0 cm"
+                  ? "Unknown height"
+                  : hero?.appearance?.height?.[1]}
+              </p>
             </span>
             <span>
-              Race: <p>{hero?.appearance?.race === "null" ? "Unknown height": hero?.appearance?.race}</p>
+              Race:{" "}
+              <p>
+                {hero?.appearance?.race === "null"
+                  ? "Unknown height"
+                  : hero?.appearance?.race}
+              </p>
             </span>
             <span>
               Publisher: <p>{hero?.biography?.publisher}</p>
@@ -79,32 +112,52 @@ export const HeroCard: React.FC<iProps> = ({ id, variant }) => {
           <HeroStats
             statsName={"int"}
             variant={"md"}
-            statsValue={hero?.powerstats?.intelligence === "null" ? "0" : hero?.powerstats?.intelligence}
+            statsValue={
+              hero?.powerstats?.intelligence === "null"
+                ? "0"
+                : hero?.powerstats?.intelligence
+            }
           />
           <HeroStats
             statsName={"str"}
             variant={"md"}
-            statsValue={hero?.powerstats?.strength === "null" ? "0" : hero?.powerstats?.strength}
+            statsValue={
+              hero?.powerstats?.strength === "null"
+                ? "0"
+                : hero?.powerstats?.strength
+            }
           />
           <HeroStats
             statsName={"spd"}
             variant={"md"}
-            statsValue={hero?.powerstats?.speed === "null" ? "0" : hero?.powerstats?.speed}
+            statsValue={
+              hero?.powerstats?.speed === "null" ? "0" : hero?.powerstats?.speed
+            }
           />
           <HeroStats
             variant={"md"}
             statsName={"dur"}
-            statsValue={hero?.powerstats?.durability === "null" ? "0" : hero?.powerstats?.durability}
+            statsValue={
+              hero?.powerstats?.durability === "null"
+                ? "0"
+                : hero?.powerstats?.durability
+            }
           />
           <HeroStats
             statsName={"pwr"}
             variant={"md"}
-            statsValue={hero?.powerstats?.power === "null" ? "0" : hero?.powerstats?.power}
+            statsValue={
+              hero?.powerstats?.power === "null" ? "0" : hero?.powerstats?.power
+            }
           />
           <HeroStats
             statsName={"cmb"}
             variant={"md"}
-            statsValue={hero?.powerstats?.combat === "null" ? "0" : hero?.powerstats?.combat}
+            statsValue={
+              hero?.powerstats?.combat === "null"
+                ? "0"
+                : hero?.powerstats?.combat
+            }
           />
         </div>
       </div>
